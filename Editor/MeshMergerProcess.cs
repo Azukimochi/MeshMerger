4fd2ee9b9846bf0f681c50a37f4f5187da537237
Azukimochi <Azukimochi25565@gmail.com>
2023-12-02 17:14:49 +0900

    initial commit

---
+++ Editor/MeshMergerProcess.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MeshMergerProcess : MonoBehaviour
+{
+    static public void MergeMeshes(GameObject[] gameObjects, string savePath)
+    {
+        // 新しいメッシュを作成
+        Mesh combinedMesh = new Mesh();
+        List<Material> materials = new List<Material>();
+
+        // CombineInstance配列とマテリアルリストを作成
+        List<CombineInstance> combineInstances = new List<CombineInstance>();
+
+        foreach (var gameObject in gameObjects)
+        {
+            if (gameObject == null) continue;
+
+            MeshRenderer meshRenderer = gameObject.GetComponent<MeshRenderer>();
+            MeshFilter mf = gameObject.GetComponent<MeshFilter>();
+
+            if (mf == null || meshRenderer == null) continue;
+
+            Material[] localMats = meshRenderer.sharedMaterials;
+            for (int materialIndex = 0; materialIndex < localMats.Length; materialIndex++)
+            {
+                materials.Add(localMats[materialIndex]);
+
+                CombineInstance ci = new CombineInstance
+                {
+                    mesh = mf.sharedMesh,
+                    subMeshIndex = materialIndex,
+                    transform = gameObject.transform.localToWorldMatrix
+                };
+                combineInstances.Add(ci);
+            }
+        }
+
+        // メッシュを統合
+        combinedMesh.CombineMeshes(combineInstances.ToArray(), false);
+
+        // 新しいメッシュにマテリアルを適用
+        GameObject combinedObject = new GameObject("CombinedMesh");
+        combinedObject.AddComponent<MeshFilter>().sharedMesh = combinedMesh;
+        var renderer = combinedObject.AddComponent<MeshRenderer>();
+        renderer.sharedMaterials = materials.ToArray();
+
+        // 統合したメッシュをアセットとして保存
+        AssetDatabase.CreateAsset(combinedMesh, savePath + ".asset");
+
+        // プレハブとして保存
+        PrefabUtility.SaveAsPrefabAsset(combinedObject, savePath + ".prefab");
+
+        // オブジェクトを削除（オプション）
+        DestroyImmediate(combinedObject);
+    }
+}
---
+++ Editor/MeshMergetGUI.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Animations;
+using VRC.Core;
+
+public class MeshMergerGUI : EditorWindow
+{
+    private const string Title = "MeshMerger";
+    
+    private Vector2 _scrollPosition = Vector2.zero;
+    
+    
+    [SerializeField]
+    private GameObject[] _gameObjects;
+    
+    [MenuItem("Tools/MeshMerger")]
+    public static void CreateWindow() => GetWindow<MeshMergerGUI>(Title);
+
+    private void OnGUI()
+    {
+           
+        EditorGUILayout.LabelField(Title, EditorStyles.boldLabel);
+
+        EditorGUILayout.Separator();
+        EditorGUILayout.LabelField("Select GameObjects to merge:");
+        
+        _scrollPosition = EditorGUILayout.BeginScrollView(_scrollPosition);
+        
+        SerializedObject serializedObject = new SerializedObject(this);
+        SerializedProperty serializedProperty = serializedObject.FindProperty("_gameObjects");
+        EditorGUILayout.PropertyField(serializedProperty, true);
+        serializedObject.ApplyModifiedProperties();
+        
+        EditorGUILayout.EndScrollView();
+        
+        EditorGUILayout.Separator();
+        
+        if (GUILayout.Button("Merge Meshes"))
+        {
+            string savePath = EditorUtility.SaveFilePanelInProject( "MergedMesh", "asset", "", "Assets");
+            MeshMergerProcess.MergeMeshes(_gameObjects, savePath);
+        }
+    }
+}
